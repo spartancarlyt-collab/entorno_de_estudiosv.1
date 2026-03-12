64a027eedacbb82e2c0c7ece5703b5cc4ffd7085
spartancarlyt-collab <spartancarlyt@gmail.com>
2026-03-12 00:17:00 -0400

    Clean up: Remove scripts, add engram.db to gitignore

--- deploy.ps1
+++
@@ -1,2 +0,0 @@
-$env:Path = "C:\Program Files\nodejs;C:\Users\carly\AppData\Roaming\npm;$env:Path"
-vercel --prod --yes
--- deploy2.ps1
+++
@@ -1,3 +0,0 @@
-$env:Path = "C:\Program Files\nodejs;C:\Users\carly\AppData\Roaming\npm;$env:Path"
-$env:NODE_PATH = "C:\Program Files\nodejs\node_modules"
-& "C:\Program Files\nodejs\node.exe" "C:\Users\carly\AppData\Roaming\npm\node_modules\vercel\bin\vercel" --prod --yes
